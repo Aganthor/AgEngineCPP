23b8ec8a78489ebfb063be3b188b933227925204
Aganthor <psyluche@gmail.com>
2018-10-15 10:34:48 -0400

    REnamed my .h files to .hpp.

--- map/Level.hpp
+++ map/Level.hpp
@@ -1,55 +1,13 @@
-#pragma once
+#pragme once
 
-#include <vector>
-#include <memory>
-
-#include "Tileinfo.hpp"
-#include "../utility/TextureManager.hpp"
-
-//TODO: level class should not hold onto a map generator...
-//TODO: It should only be passed by reference within the generateLevel function.
-
-
-//https://codereview.stackexchange.com/questions/87367/texture-managing
-//http://gameprogrammingpatterns.com/service-locator.html
-
-namespace sf
-{
-	class RenderWindow;
-}
+//
+//A Level class to hold information concerning a level i.e. map, objects.
+//
 
 namespace map
 {
-	//
-	//Forward declarations
-	//
-	class BaseMapGenerator;
+    class Level
+    {
 
-
-	//
-	// Level class will represent the actual level to be used for logic
-	// and rendering. 
-	//
-	class Level
-	{
-	public:
-		Level();
-		~Level();
-
-		void generateLevel(BaseMapGenerator& mapGenerator);
-		void renderLevel(sf::RenderWindow& window);
-		//void saveLevel();
-		//void loadLevel();
-
-		TileInfo& getTileInfo(int x, int y);
-
-	private:
-		void loadTextureFiles();
-		
-	private:
-		using TilePtr = std::unique_ptr<TileInfo>;
-		std::vector<TilePtr> m_levelData;
-
-		TextureManager m_textureManager;
-	};
+    };
 }

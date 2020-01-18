a0d6ca679e46b311ee2b4b3d7825fc9d77ba0ec0
Luc Bergeron <psyluche@gmail.com>
2020-01-17 23:10:21 -0500

    Replaced all the emplace_back with push_back.

--- map/Level.cpp
+++ map/Level.cpp
@@ -21,8 +21,6 @@ namespace map
         m_levelWidth = mapGenerator.getMapWidth();
         int levelHeight = mapGenerator.getMapHeight();
 
-        m_levelData.resize(m_levelWidth * levelHeight);
-
         for (auto y = 0; y < levelHeight; ++y)
 		{
             for (auto x = 0; x < m_levelWidth; ++x)
@@ -31,35 +29,35 @@ namespace map
 
 				if ((mapValue >= -1.0) && (mapValue < -0.25)) //deep water
 				{
-					m_levelData.emplace_back(std::make_unique<TileInfo>(x, y, TileType::DEEP_WATER, false, false, false));
+					m_levelData.push_back(std::make_unique<TileInfo>(x, y, TileType::DEEP_WATER, false, false, false));
 				}
 				else if ((mapValue >= -0.25) && (mapValue < 0.0)) //shallow water
 				{
-					m_levelData.emplace_back(std::make_unique<TileInfo>(x, y, TileType::SHALLOW_WATER, false, false, false));
+					m_levelData.push_back(std::make_unique<TileInfo>(x, y, TileType::SHALLOW_WATER, false, false, false));
 				}
 				else if ((mapValue >= 0.0) && (mapValue < 0.0625)) //shore
 				{
-					m_levelData.emplace_back(std::make_unique<TileInfo>(x, y, TileType::SHORE, false, false, true));
+					m_levelData.push_back(std::make_unique<TileInfo>(x, y, TileType::SHORE, false, false, true));
 				}
 				else if ((mapValue >= 0.0625) && (mapValue < 0.1250)) //sand
 				{
-					m_levelData.emplace_back(std::make_unique<TileInfo>(x, y, TileType::SAND, false, false, true));
+					m_levelData.push_back(std::make_unique<TileInfo>(x, y, TileType::SAND, false, false, true));
 				}
 				else if ((mapValue >= 0.1250) && (mapValue < 0.3750)) //grass
 				{
-					m_levelData.emplace_back(std::make_unique<TileInfo>(x, y, TileType::GRASS, false, false, true));
+					m_levelData.push_back(std::make_unique<TileInfo>(x, y, TileType::GRASS, false, false, true));
 				}
 				else if ((mapValue >= 0.3750) && (mapValue < 0.75)) //dirt
 				{
-					m_levelData.emplace_back(std::make_unique<TileInfo>(x, y, TileType::DIRT, false, false, true));
+					m_levelData.push_back(std::make_unique<TileInfo>(x, y, TileType::DIRT, false, false, true));
 				}
 				else if ((mapValue >= 0.75) && (mapValue < 1.0)) //rock
 				{
-					m_levelData.emplace_back(std::make_unique<TileInfo>(x, y, TileType::ROCK, false, false, false));
+					m_levelData.push_back(std::make_unique<TileInfo>(x, y, TileType::ROCK, false, false, false));
 				}
 				else //snow
 				{
-					m_levelData.emplace_back(std::make_unique<TileInfo>(x, y, TileType::SNOW, false, false, false));
+					m_levelData.push_back(std::make_unique<TileInfo>(x, y, TileType::SNOW, false, false, false));
 				}
 			}
 		}
@@ -71,9 +69,10 @@ namespace map
 
 		for (auto &tileInfo: m_levelData)
         {
-            sprite.setTexture(m_textureManager.getTexture(TileType::GRASS));
-            //TODO : doesn't seem to work!
-            sprite.setPosition((*tileInfo).x, (*tileInfo).y);
+            sprite.setTexture(m_textureManager.getTexture((*tileInfo).tileType));
+			int x = (*tileInfo).x * TILE_SIZE;
+			int y = (*tileInfo).y * TILE_SIZE;
+            sprite.setPosition(x, y);
             window.draw(sprite);
 
 		}
